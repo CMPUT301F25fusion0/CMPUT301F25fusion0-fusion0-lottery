79212f3d370e23a9b672c83e52f7495496cc73d4
Joshua Li <joshua12@ualberta.ca>
2025-10-27 22:33:45 -0600

    sign up, add, delete

--- app/src/main/java/com/example/fusion0_lottery/MainActivity.java
+++ app/src/main/java/com/example/fusion0_lottery/MainActivity.java
@@ -2,36 +2,34 @@ package com.example.projectfusion0;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-
-import com.google.firebase.firestore.CollectionReference;
+import com.example.fusion0_lottery.Users;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    /*
-    // random starter variables
-    private EditText usernameInput;
-    private EditText emailInput;
-    private EditText phoneInput;
-    private Button buttonName;
-    private String username;
-    private String email;
-    private String phone;
-    */
+    // sign up inputs
+    private EditText usernameInput, emailInput, phoneInput, passwordInput, confirmPasswordInput;
 
+    // button for the sign up button
+    private Button buttonSignUp;
+
+    // when user is created in the database, store the document ID
+    protected String documentID;
     private FirebaseFirestore db;
 
     @Override
@@ -39,62 +37,86 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         db = FirebaseFirestore.getInstance();
 
-        // below is a test example to see if info is added to database (working)
-        Map<String, Object> user1 = new HashMap<>();
-        user1.put("username", "test");
-        user1.put("email", "email@gmail.com");
-        user1.put("phone", "123");
+        usernameInput = findViewById(R.id.usernameInput);
+        emailInput = findViewById(R.id.emailInput);
+        phoneInput = findViewById(R.id.phoneInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        confirmPasswordInput = findViewById(R.id.confirmPasswordInput);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
 
-        db.collection("users")
-                .add(user1)
-                .addOnSuccessListener(documentReference -> Log.d("Firestore", "Profile Created: " + documentReference.getId()))
-                .addOnFailureListener(e -> Log.w("Firestore", "Error Creating Profile", e));
+        buttonSignUp.setOnClickListener(v -> {
+            String username = usernameInput.getText().toString();
+            String email = emailInput.getText().toString();
+            String phone = phoneInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            String confirmPassword = confirmPasswordInput.getText().toString();
 
+            // some app pop-up messages if sign up isn't done correctly goes below
+            // feel free to add some yourselves
 
-        // below is a test example to see if user2 is added and then delete (working)
-        Map<String, Object> user2 = new HashMap<>();
-        user2.put("username", "testDelete");
-        user2.put("email", "delete@gmail.com");
-        user2.put("phone", "789");
+            // check if name, email, and password are filled
+            if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Provide necessary information", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
-        db.collection("users")
-                .add(user2)
-                .addOnSuccessListener(documentReference -> {
-                    Log.d("Firestore", "Profile Created: " + documentReference.getId());
-                    db.collection("users").document(documentReference.getId())
-                            .delete()
-                            .addOnSuccessListener(aVoid -> Log.d("Firestore", "User2 successfully deleted!"))
-                            .addOnFailureListener(e -> Log.w("Firestore", "Error deleting User2", e));
+            // check if password = confirm password
+            if (!password.equals(confirmPassword)) {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // if sign up information is correctly inputted, add user to the database
+            addUserToDatabase(username, email, phone, password);
+        });
+    }
+
+    /**
+     * this function adds users to the database
+     * @param username is the user's username to be added
+     * @param email is the user's email to be added
+     * @param phone is the user's phone to be added
+     * @param password is the user's password to be added
+    */
+    private void addUserToDatabase(String username, String email, String phone, String password) {
+        Users user = new Users(username, email, phone, password);
+
+        db.collection("Users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // store the documentID for that specific user (used to delete user account)
+                        documentID = documentReference.getId();
+                        Toast.makeText(MainActivity.this, "Sign-up successful!", Toast.LENGTH_SHORT).show();
+                        // clear information after signing up
+                        usernameInput.setText("");
+                        emailInput.setText("");
+                        phoneInput.setText("");
+                        passwordInput.setText("");
+                        confirmPasswordInput.setText("");
+                    }
                 })
-                .addOnFailureListener(e -> Log.w("Firestore", "Error Creating Profile", e));
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    // function to delete user from the database (hasn't been tested yet)
+    private void deleteUserFromDatabase() {
+        // using the documentID stored from when user was created, delete the ID and the user
+        db.collection("Users").document(documentID).delete()
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(MainActivity.this, "Successfully deleted user", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(MainActivity.this, "Error deleting user: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 }
-
-/*
-// idk some random code that might work template
-private void addUserToDatabase(String username, String email, String phoneNumber) {
-    CollectionReference UsersDB = db.collection("Users");
-    Users user = new Users(username, email, phoneNumber);
-
-    // add user to Users collection
-     UsersDB.add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-        @Override
-        public void onSuccess(DocumentReference documentReference) {
-            Toast.makeText(MainActivity.this, "User has successfully been added", Toast.LENGTH_SHORT).show();
-        }
-     }).addOnFailureListener(new OnFailureListener() {
-        @Override
-        public void onFailure(@NonNull Exception e) {
-            Toast.makeText(MainActivity.this, "User Creation Failed" + e, Toast.LENGTH_SHORT).show();
-        }
-     });
-}
-*/
