31ac79d31d9999429a5ef02b1ccfc3a4b371b676
Micho0310 <mineobrine11@gmail.com>
2025-10-28 18:17:21 -0600

    Added test events to check MainActivityOrganizer (not finished)

--- app/src/main/java/com/example/fusion0_lottery/MainActivityOrganizer.java
+++ app/src/main/java/com/example/fusion0_lottery/MainActivityOrganizer.java
@@ -1,6 +1,8 @@
 package com.example.fusion0_lottery;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.projectfusion0.R;
 import com.google.firebase.firestore.auth.User;
 
+import java.util.ArrayList;
+
 public class MainActivityOrganizer extends AppCompatActivity {
 
     private Users organizer;
@@ -20,6 +24,21 @@ public class MainActivityOrganizer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.main_activity_organizer);
+
+        // Test events
+        Event fortnite = new Event("fortnite",
+                "gaming event", "march",
+                "april", "6:07",
+                6.7, "edmonton",
+                "feb", "march",
+                2);
+        ArrayList<Event> testEvent = new ArrayList<>();
+        testEvent.add(fortnite);
+
+        ListView eventsOrg = findViewById(R.id.eventsOrg);
+        ArrayAdapter<Event> testAdapter = new ArrayAdapter<>(this, R.layout.event_layout, testEvent);
+        eventsOrg.setAdapter(testAdapter);
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
