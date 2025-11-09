84f391288de1d3ce1575fbe356f18ba0330b08e2
merci63 <mersimoyt27@gmail.com>
2025-11-08 20:56:36 -0700

    Implemented MyEvent activity which shows waitlist and selected events for user in horizontally scroll view

--- app/src/main/java/com/example/fusion0_lottery/MyEventAdapter.java
+++ app/src/main/java/com/example/fusion0_lottery/MyEventAdapter.java
@@ -1,4 +1,82 @@
 package com.example.fusion0_lottery;
 
-public class MyEventAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyEventAdapter extends RecyclerView.Adapter<MyEventAdapter.MyViewHolder> {
+    private List<Event> events;
+    private String type;
+
+    public MyEventAdapter(List<Event> events, String type){
+        this.events = events;
+        this.type = type;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        int layout_res;
+        if (type.equals("waiting")) {
+            layout_res = R.layout.waiting;
+        } else {
+            layout_res = R.layout.selected;
+        }
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(layout_res, parent, false);
+        return new MyViewHolder(view, type);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Event event = events.get(position);
+
+        String eventName = event.getEventName();
+        String drawDate = event.getRegistrationEnd();
+
+        if (type.equals("waiting")){
+            holder.eventName.setText(eventName);
+            holder.eventStatus.setText("Status: on waiting list");
+            holder.drawDate.setText("Draw: "+ drawDate);
+        } else if (type.equals("selected")) {
+            holder.selectedName.setText(eventName);
+            holder.accept.setOnClickListener(v->{
+
+            });
+            holder.decline.setOnClickListener(v->{
+
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView eventName, eventStatus, drawDate, selectedName;
+        Button accept, decline;
+
+        public MyViewHolder(@NonNull View itemView, String type) {
+            super(itemView);
+
+            if (type.equals("waiting")) {
+                eventName = itemView.findViewById(R.id.event_name);
+                eventStatus = itemView.findViewById(R.id.event_status);
+                drawDate = itemView.findViewById(R.id.draw_date);
+            } else if (type.equals("selected")) {
+                selectedName = itemView.findViewById(R.id.selected_event_name);
+                accept = itemView.findViewById(R.id.accept);
+                decline = itemView.findViewById(R.id.decline);
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/fusion0_lottery/MyEventsActivity.java
+++ app/src/main/java/com/example/fusion0_lottery/MyEventsActivity.java
@@ -1,24 +1,145 @@
 package com.example.fusion0_lottery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyEventsActivity extends AppCompatActivity {
+
+    private RecyclerView recycler_waiting;
+    private RecyclerView recycler_selected;
+
+    private FirebaseFirestore db;
+    private FirebaseUser current_user;
+    private List<Event> waiting_events;
+    private List<Event> selected_event;
+    private MyEventAdapter waitingAdapter;
+    private MyEventAdapter selectedAdapter;
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_my_events);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        db = FirebaseFirestore.getInstance();
+        current_user = FirebaseAuth.getInstance().getCurrentUser();
+
+        recycler_waiting = findViewById(R.id.recycler_waiting);
+        recycler_selected = findViewById(R.id.recycler_selected);
+
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        setupBottomNavigation();
+
+
+        waiting_events = new ArrayList<>();
+        selected_event = new ArrayList<>();
+
+
+        horizontal_scroll(recycler_waiting);
+        horizontal_scroll(recycler_selected);
+
+        waitingAdapter = new MyEventAdapter(waiting_events,"waiting");
+        selectedAdapter = new MyEventAdapter(selected_event,"selected");
+
+
+
+        recycler_waiting.setAdapter(waitingAdapter);
+        recycler_selected.setAdapter(selectedAdapter);
+        load_events();
+    }
+
+    private void horizontal_scroll(RecyclerView recycler) {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recycler.setLayoutManager(layoutManager);
+    }
+
+
+    private void load_events() {
+
+        waiting_events.clear();
+        selected_event.clear();
+        updateAdapters();
+
+        db.collection("Events").whereArrayContains("waitingList", current_user
+                .getUid()).get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+            for(QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                Event event = snapshot.toObject(Event.class);
+                event.setEventId(snapshot.getId());
+                waiting_events.add(event);
+            }
+
+            updateAdapters();
+        });
+        db.collection("Events").whereArrayContains("selectedUser",current_user
+                .getUid()).get()
+                .addOnSuccessListener(queryDocumentSnapshots ->{
+            for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                 Event event = document.toObject(Event.class);
+                 event.setEventId(document.getId());
+                 selected_event.add(event);
+            }
+
+            updateAdapters();
         });
     }
+
+    private void updateAdapters() {
+        if(waitingAdapter != null){
+            waitingAdapter.notifyDataSetChanged();
+        }
+        if (selectedAdapter != null){
+            selectedAdapter.notifyDataSetChanged();
+        }
+    }
+    private void setupBottomNavigation() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.navigation_home) {
+                Intent intent = new Intent(MyEventsActivity.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (itemId == R.id.navigation_my_events) {
+                return true;
+            } else if (itemId == R.id.navigation_profile) {
+
+//                Intent intent = new Intent(MyEventsActivity.this, ProfileActivity.class);
+//                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.navigation_my_events);
+    }
+
 }
